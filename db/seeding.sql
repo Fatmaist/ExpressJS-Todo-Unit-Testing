a1ba4518551fe1dbcc218cfa49ef6c9492d6f244
fatmaist <fatmaistyn@gmail.com>
2023-10-28 09:58:07 +0700

    feat(db) : Create Table Todo and insert values to database

---
+++ db/seeding.sql
@@ -0,0 +1,5 @@
+INSERT INTO todo (id, task, date, status) VALUES 
+(1, 'Send email', '2023-10-10', 'Completed'),
+(2, 'Water the plant', '2023-10-15', 'Completed'),
+(3, 'Learn SQL', '2023-10-24', 'In Progress'),
+(4, 'Buy milk', '2023-10-28', 'Not Started')
---
+++ migrations/sqls/20231028023600-initialize-up.sql
@@ -0,0 +1,9 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE todo (
+    id INT PRIMARY KEY,
+    task VARCHAR(255),
+    date DATE,
+    status VARCHAR(50)
+)
+
