90d16fb87519b5a43117c95377ad213a71c6dd09
fatmaist <fatmaistyn@gmail.com>
2023-10-28 09:32:22 +0700

    feat(db) : Create the connection to database with PostgreSQL

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
+var pool = require('./queries')
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+pool.connect((err, res) => {
+    console.log(err)
+    console.log('connected')
 })
 
 app.listen(3000)
